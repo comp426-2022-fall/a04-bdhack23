265d1b7a5f4f7f1615712ca9ed3f5057f316d36f
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-11-04 18:08:34 -0400

    made requirments

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+// Require express module
+const http = require('express');
+
+// Require minimist module
+const args = require('minimist')(process.argv.slice(2));
+
+// Use minimist to process one argument `--port=` on the command line after `node server.js`.
+args["port"];
+
+// Define a const `port` using the argument from the command line. 
+// Make this const default to port 5000 if there is no argument given for `--port`.
+const port = args.port || process.env.PORT || 5000;
 
